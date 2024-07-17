7f171b0e4a72f617faa5b4630d50fed561a7e761
IT阿杰 <107783255+cosyjie@users.noreply.github.com>
2024-07-17 18:13:31 +0800

    =

--- menu.py
+++ menu.py
@@ -1,12 +1,10 @@
-from django.urls import reverse
-
 menu = {
     'module_system': {
         'child': [
             {
                 'name': 'system_firewalld',
                 'title': '防火墙',
-                'href': reverse('module_system:system_firewalld:port-list'),
+                'href': 'module_system:system_firewalld:port-list',
             },
         ]
     }
